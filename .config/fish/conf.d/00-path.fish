aa9e117bb62b917ad4d27728e51038cf313382e2
Christopher Sarmiento <chris@topher.ai>
2026-02-01 20:32:40 -0500

    add bob to path

--- .config/fish/conf.d/00-path.fish
+++ .config/fish/conf.d/00-path.fish
@@ -1,3 +1,8 @@
 fish_add_path /opt/homebrew/bin
 fish_add_path /opt/homebrew/sbin
 fish_add_path $HOME/.local/bin
+fish_add_path $HOME/.local/share/bob/nvim-bin
+
+if type -q go
+    fish_add_path (go env GOPATH)/bin
+end
