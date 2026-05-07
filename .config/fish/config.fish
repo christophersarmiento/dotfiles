03a7d183ac8f652f5923e79fafa8b38ba3577743
Christopher Sarmiento <chris@topher.ai>
2026-05-07 10:21:29 -0400

    chore: remove conflicting files

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -3,15 +3,12 @@ alias vim="nvim"
 alias cd="z"
 alias ls="eza --icons=always -G"
 alias ll="eza --icons=always -l -a -h --smart-group --git --git-repos-no-status"
-alias convert-flac="uv run /Users/christopher/code/scripts/convert.py /Users/christopher/Music/Downloads/flacs"
 alias cat="bat"
 
 # Source machine specific variables
 if test -f ~/.config/fish/.profile.fish
     source ~/.config/fish/.profile.fish
 end
-
-set -gx HELIX_RUNTIME ~code/helix/src/helix/runtime
 
 # Fuzzy finder
 fzf --fish | source
